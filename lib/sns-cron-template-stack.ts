276a1c7bdf85f41fd80e0a581995a57483a2878a
Matthew Pagan <mastapegs01@gmail.com>
2023-09-30 09:04:42 -0400

    update logic and remove unneeded code

--- lib/sns-cron-template-stack.ts
+++ lib/sns-cron-template-stack.ts
@@ -25,17 +25,11 @@ export class SnsCronTemplateStack extends Stack {
     });
 
     // Create an EventBridge rule that is scheduled to run twice a day.
-    const rule1 = new aws_events.Rule(this, "TextMessagingRule9", {
-      schedule: aws_events.Schedule.cron({ hour: "9", minute: "0" }),
-    });
-
-    const rule2 = new aws_events.Rule(this, "TextMessagingRule21", {
-      schedule: aws_events.Schedule.cron({ hour: "21", minute: "0" }),
-    });
-
-    const rule3 = new aws_events.Rule(this, "TextMessagingRuleTest", {
-      schedule: aws_events.Schedule.cron({ hour: "1", minute: "20" }),
-    });
+    const [rule1, rule2] = ["9", "21"].map((hour) =>
+      new aws_events.Rule(this, `RuleForHour${hour}`, {
+        schedule: aws_events.Schedule.cron({ hour, minute: "0" }),
+      })
+    );
 
     const lambdaFunction = new aws_lambda_nodejs.NodejsFunction(
       this,
@@ -44,17 +38,6 @@ export class SnsCronTemplateStack extends Stack {
         runtime: aws_lambda.Runtime.NODEJS_18_X,
       },
     );
-
-    // Create a Lambda function that is triggered by the EventBridge rule.
-    // const lambdaFunction = new aws_lambda.Function(
-    //   this,
-    //   "TextMessagingLambda",
-    //   {
-    //     runtime: aws_lambda.Runtime.NODEJS_LATEST,
-    //     handler: "handler/lambda.js",
-    //     code: aws_lambda.Code.fromAsset("handler"),
-    //   },
-    // );
 
     // Add a permission statement to the Lambda function to allow it to publish messages to the SNS topic.
     lambdaFunction.addToRolePolicy(
@@ -65,7 +48,7 @@ export class SnsCronTemplateStack extends Stack {
     );
 
     // Add a target to the EventBridge rule to trigger the Lambda function.
-    [rule1, rule2, rule3].map((rule) =>
+    [rule1, rule2].map((rule) =>
       rule.addTarget(new aws_events_targets.LambdaFunction(lambdaFunction))
     );
   }
