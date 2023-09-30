f535fa322803b5c5a93f76b6656a6bf2af89dc9f
Matthew Pagan <mastapegs01@gmail.com>
2023-09-30 10:11:30 -0400

    adding back sns for sms

--- lib/sns-cron-template-stack.ts
+++ lib/sns-cron-template-stack.ts
@@ -1,8 +1,10 @@
 import {
   aws_events,
   aws_events_targets,
+  aws_iam,
   aws_lambda,
   aws_lambda_nodejs,
+  aws_sns,
   Stack,
   StackProps,
 } from "aws-cdk-lib";
@@ -11,6 +13,16 @@ import { Construct } from "constructs";
 export class SnsCronTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // Create an SNS topic.
+    const topic = new aws_sns.Topic(this, "TextMessagingTopic");
+
+    // Subscribe the user's phone number to the SNS topic.
+    new aws_sns.Subscription(this, "TextMessagingSubscription", {
+      topic,
+      protocol: aws_sns.SubscriptionProtocol.SMS,
+      endpoint: "+14013327084",
+    });
 
     // Create an EventBridge rule that is scheduled to run twice a day.
     const EST_TO_UTC_OFFSET = 4;
@@ -32,6 +44,14 @@ export class SnsCronTemplateStack extends Stack {
       {
         runtime: aws_lambda.Runtime.NODEJS_18_X,
       },
+    );
+
+    // Add a permission statement to the Lambda function to allow it to publish messages to the SNS topic.
+    lambdaFunction.addToRolePolicy(
+      new aws_iam.PolicyStatement({
+        actions: ["sns:Publish"],
+        resources: [topic.topicArn],
+      }),
     );
 
     // Add a target to the EventBridge rule to trigger the Lambda function.
