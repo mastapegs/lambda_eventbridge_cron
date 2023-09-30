81dafd5a7f3929d30681e338e8e80269e2294f9f
Matthew Pagan <mastapegs01@gmail.com>
2023-09-30 09:45:43 -0400

    remove sns, will use twilio for sms

--- lib/sns-cron-template-stack.ts
+++ lib/sns-cron-template-stack.ts
@@ -1,10 +1,8 @@
 import {
   aws_events,
   aws_events_targets,
-  aws_iam,
   aws_lambda,
   aws_lambda_nodejs,
-  aws_sns,
   Stack,
   StackProps,
 } from "aws-cdk-lib";
@@ -13,16 +11,6 @@ import { Construct } from "constructs";
 export class SnsCronTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // Create an SNS topic.
-    const topic = new aws_sns.Topic(this, "TextMessagingTopic");
-
-    // Subscribe the user's phone number to the SNS topic.
-    new aws_sns.Subscription(this, "TextMessagingSubscription", {
-      topic,
-      protocol: aws_sns.SubscriptionProtocol.SMS,
-      endpoint: "+14013327084",
-    });
 
     // Create an EventBridge rule that is scheduled to run twice a day.
     const [rule1, rule2] = ["9", "21"].map((hour) =>
@@ -37,14 +25,6 @@ export class SnsCronTemplateStack extends Stack {
       {
         runtime: aws_lambda.Runtime.NODEJS_18_X,
       },
-    );
-
-    // Add a permission statement to the Lambda function to allow it to publish messages to the SNS topic.
-    lambdaFunction.addToRolePolicy(
-      new aws_iam.PolicyStatement({
-        actions: ["sns:Publish"],
-        resources: [topic.topicArn],
-      }),
     );
 
     // Add a target to the EventBridge rule to trigger the Lambda function.
